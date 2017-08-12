d9897d062edef6814c4dfda8a6be5ded5b3b6d2e
slavafox2 <tyop@tut.by>
2017-08-12 10:13:13 +0300

    Implemented form authentication and custom role provider

--- SimpleBlog/Controllers/AuthController.cs
+++ SimpleBlog/Controllers/AuthController.cs
@@ -4,30 +4,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace SimpleBlog.Controllers
 {
     public class AuthController : Controller
     {
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToRoute("Home");
+
+        }
         public ActionResult Login()
         {
             return View();
         }
         
         [HttpPost]
-        public ActionResult Login(AuthLogin form)
+        public ActionResult Login(AuthLogin form, string returnUrl)
         {
             if (!ModelState.IsValid)
             {
                 return View(form);
             }
-            if (form.Username != "rainbow")
-            {
-                ModelState.AddModelError("Username", "Username of passsword isn't 20% cooler");
+
+            FormsAuthentication.SetAuthCookie(form.Username, true); //Authentication
+
+            if (!string.IsNullOrWhiteSpace(returnUrl)){
+                return Redirect(returnUrl);
             }
-
-
-            return Content("The form is valid");
+            return RedirectToRoute("Home");
         }
     }
 
