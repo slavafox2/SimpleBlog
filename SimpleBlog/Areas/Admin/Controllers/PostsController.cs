1dea618463fc01293b37598f3cad3de921928831
slavafox2 <tyop@tut.by>
2017-08-23 18:45:41 +0300

    Fixed SELECT  n+1 IN posts admin and updated Framwork

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -15,16 +15,24 @@ namespace SimpleBlog.Areas.Admin.Controllers
     [SelectedTab("posts")]
     public class PostsController : Controller
     {
-        private const int PostsPerPage = 5;
+        private const int PostsPerPage = 6;
 
         public ActionResult Index(int page = 1)
         {
             var totalPostCount = Database.Session.Query<Post>().Count();
 
-            var currentPostPage = Database.Session.Query<Post>()
-                .OrderBy(c => c.CraetedAt)
+            var baseQuery = Database.Session.Query<Post>().OrderByDescending(f => f.CraetedAt);
+
+            var postIds = baseQuery
                 .Skip((page - 1) * PostsPerPage)
                 .Take(PostsPerPage)
+                .Select(p => p.Id)
+                .ToArray();
+
+            var currentPostPage = baseQuery
+                .Where(p => postIds.Contains(p.Id))
+                .Fetch(f => f.Tags)
+                .Fetch(f=>f.User)                               
                 .ToList();
 
             return View(new PostsIndex
